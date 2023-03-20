86bbce6143bcccdc027035b448ab7518254d59c1
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-03-20 23:25:20 +0300

    task_011_three
    Signed-off-by: kondr799 <83312253+kondr799@users.noreply.github.com>

--- task_011/Program.cs
+++ task_011/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Console.Clear();
+// [100, 999]
+int n = new Random().Next(100, 1000);
+int n1 = n / 100;
+int n2 = n % 10;
+
+Console.WriteLine($"random = {n}");
+
+Console.WriteLine(n1 * 10 + n2);
