62214a87e89f2f4363d81a5533fe63a98dc99033
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-03-21 00:12:37 +0300

    task_014_account
    Signed-off-by: kondr799 <83312253+kondr799@users.noreply.github.com>

---
+++ task_014/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет
+
+Console.Clear();
+Console.Write("Введите 1 число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 * n1 == n2 || n2 * n2 == n1)
+{
+    Console.WriteLine($"Да");
+}
+else
+    Console.WriteLine($"Нет");
