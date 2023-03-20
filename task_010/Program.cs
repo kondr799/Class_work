f890c975f5867847d0d55b3e282f790a50ab21a4
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-03-20 22:35:06 +0300

    task_010_random
    Signed-off-by: kondr799 <83312253+kondr799@users.noreply.github.com>

---
+++ task_010/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Например:
+
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+Console.Clear();
+int n = new Random().Next(10, 99); // [10, 99]
+int n1 = n / 10;
+int n2 = n % 10;
+Console.WriteLine($"random = {n}");
+
+if (n1 > n2)
+Console.WriteLine(n1);
+else if (n1 < n2)
+Console.WriteLine(n2);
+else
+Console.WriteLine("=");
