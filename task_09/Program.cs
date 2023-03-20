c18442a10aa63156f3587ffdb3a18e57fa3dd401
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-03-20 07:48:00 +0300

    task_09_function
    Signed-off-by: kondr799 <83312253+kondr799@users.noreply.github.com>

---
+++ task_09/Program.cs
@@ -0,0 +1,19 @@
+﻿// function
+
+Console.Clear();
+
+int f(int x, int y)
+{
+    if (x > y) return x;
+
+    else
+    {
+        return y;
+    }
+}
+
+int n = 5;
+int n1 = 10;
+
+int max = f(n, n1);
+Console.WriteLine(max);
