e756909bdcf6280a5db3e0fc51ff3b0f7de5de80
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-04-10 00:03:35 +0300

    task_053_class

---
+++ task_053/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+Console.Clear();
+
+int[,] InputMatrix()
+{
+    Console.Write("Введите  размер матрицы: ");
+    int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+    int[,] matrix = new int[size[0], size[1]];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 60);
+            Console.Write($"{matrix[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+
+void ChangeArray(int [,] matrix)
+{
+    int temp;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        temp = matrix[0,i];
+        matrix[0,i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp;
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+    }
+}
+
+Console.Write("Введите  размер матрицы: ");
+    //int[] Position = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+    //int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+    //int[,] matrix = new int[];
+    int[,] matrix = InputMatrix();
+    ChangeArray(matrix);
+    Console.WriteLine();
+    PrintArray(matrix);
