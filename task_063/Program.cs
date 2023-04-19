b7a602cfcd48597348c16d7b1f6bfb9c8e6cae12
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-04-19 20:59:14 +0300

    task_063_class

---
+++ task_063/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Clear();
+
+string PutNumber(int Number)
+{
+    if (Number == 1)//F(5)
+    return 1.ToString();
+    string Empty = String.Empty;
+    return Empty + PutNumber(Number - 1) + ", " + Number.ToString();
+    // Рекурсия вниз, вычисление по методу:
+    // F(5)=" + F(4),_ "5 '= '1,_2,_3,_4,_5 '
+    // F(4)=" + F(3),_ "4 '= '1,_2,_3,_4 '
+    // F(3)=" + F(2),_ "3 '= '1,_2,_3 '
+    // F(2)=" + F(1),_ "2 '= ' 1,_2'
+    // F(1) = 1; возврат вверх + конкатенация.
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write(PutNumber(number));
+
