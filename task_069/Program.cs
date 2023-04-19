777ea27aa67d5cf50af1e0605bc99b712eeea05d
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-04-19 22:07:26 +0300

    task_069_class

---
+++ task_069/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8 
+
+Console.Clear();
+
+int Pow(int a, int b)
+{
+    if (b == 0) return 1;
+    return a * Pow(a, b -1);
+}
+
+Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(Pow(a, b));
+// A^B = A * A^(B - 1)
