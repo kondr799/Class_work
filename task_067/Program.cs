f00066b9e533144061d2f74e08b88de8e0c803a4
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-04-19 21:36:54 +0300

    task_067_class

---
+++ task_067/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+
+int SumDigital(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    return number % 10 + SumDigital(number / 10);
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(SumDigital(number));
+
+//Рекурсия: 123 -> 12 -> 1 -> 0.
